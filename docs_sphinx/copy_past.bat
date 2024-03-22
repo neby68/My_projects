89fa6ab5cfb5422b56d4166a3636bf1dd82faba8
Nicolas EBY <n.eby@digitsole.com>
2024-03-22 22:45:39 +0100

    [Minor - Update] split folder for sphinx documentation

---
+++ docs_sphinx/copy_past.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Copie des dossiers en cours...
+
+rem Spécifiez les chemins complets des dossiers source et de destination
+set "sourceFolder1=C:\Users\NicolasEBY\Documents\GitHub\My_projects\docs_sphinx\_build"
+set "destinationFolder=C:\Users\NicolasEBY\Documents\GitHub\My_projects\docs"
+
+rem Assurez-vous que le dossier de destination existe
+mkdir "%destinationFolder%" 2>nul
+
+rem Copiez les dossiers vers le dossier de destination
+xcopy /E /I "%sourceFolder1%" "%destinationFolder%\"
+
+echo Copie terminée.
+pause
