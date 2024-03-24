22bfa92ee4956d85fcb7fb162527c621644976c4
Nicolas EBY <n.eby@digitsole.com>
2024-03-24 08:54:05 +0100

    [Major - Clean] Clean documentation folder

--- docs_sphinx/copy_past.bat
+++ docs_sphinx/copy_past.bat
@@ -2,7 +2,7 @@
 echo Copie des dossiers en cours...
 
 rem Spécifiez les chemins complets des dossiers source et de destination
-set "sourceFolder1=C:\Users\NicolasEBY\Documents\GitHub\My_projects\docs_sphinx\_build"
+set "sourceFolder1=C:\Users\NicolasEBY\Documents\GitHub\My_projects\docs_sphinx\_build\html"
 set "destinationFolder=C:\Users\NicolasEBY\Documents\GitHub\My_projects\docs"
 
 rem Assurez-vous que le dossier de destination existe
